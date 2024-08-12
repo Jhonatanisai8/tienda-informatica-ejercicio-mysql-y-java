e19fbd309a1bc98d9cb3e25178dc5ce95cbc815f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-12 15:05:45 -0500

    métodos para insertar un producto

--- src/main/java/com/jhonatan/tiendainformatica/ImplementacioDao/DaoImpleProducto.java
+++ src/main/java/com/jhonatan/tiendainformatica/ImplementacioDao/DaoImpleProducto.java
@@ -1,10 +1,16 @@
 package com.jhonatan.tiendainformatica.ImplementacioDao;
 
+import com.jhonatan.tiendainformatica.dao.Conexion;
 import com.jhonatan.tiendainformatica.dao.ProductoDao;
 import com.jhonatan.tiendainformatica.datos.Producto;
 import java.util.List;
+import java.sql.*;
 
-public class DaoImpleProducto implements ProductoDao{
+public class DaoImpleProducto implements ProductoDao {
+    //llamamos a la instancia de la conexion
+
+    private final Conexion conexion = Conexion.getInstance();
+    private final String INSERT_PRODUCTO = "INSERT INTO producto (nombre,precio,id_fabricante) VALUES (?,?,?)";
 
     @Override
     public List<Producto> listarProductos() {
@@ -13,7 +19,20 @@ public class DaoImpleProducto implements ProductoDao{
 
     @Override
     public int insertarProducto(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int registros = 0;
+        try {
+            Connection conectar = conexion.conectarBaseDatos();
+            PreparedStatement consultaPreparada = conectar.prepareStatement(INSERT_PRODUCTO);
+            consultaPreparada.setString(1, producto.getNombre());
+            consultaPreparada.setDouble(2, producto.getPrecio());
+            consultaPreparada.setInt(3, producto.getIdFabricante());
+            registros = consultaPreparada.executeUpdate();
+            System.out.println("producto registrado");
+            conexion.desconectarBaseDatos();
+        } catch (SQLException e) {
+            System.out.println("error al insertar producto: " + e.getMessage());
+        }
+        return registros;
     }
 
     @Override
--- src/main/java/com/jhonatan/tiendainformatica/datos/Producto.java
+++ src/main/java/com/jhonatan/tiendainformatica/datos/Producto.java
@@ -2,8 +2,58 @@ package com.jhonatan.tiendainformatica.datos;
 
 public class Producto {
 
-   
-    
-    
+    private int idProducto;
+    private String nombre;
+    private double precio;
+    private int idFabricante;
+
+    public Producto(int idProducto, String nombre, double precio, int idFabricante) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.idFabricante = idFabricante;
+    }
+
+    public Producto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Producto(String nombre, double precio, int idFabricante) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.idFabricante = idFabricante;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getIdFabricante() {
+        return idFabricante;
+    }
+
+    public void setIdFabricante(int idFabricante) {
+        this.idFabricante = idFabricante;
+    }
 
 }
